a0f48861ef9e1c05e8fb53837f7f4a3410fb7c35
Meera-Ramkumar <meeraramkumar6@gmail.com>
2023-11-04 20:47:42 +0530

    classes1

--- classes.py
+++ classes.py
@@ -1,8 +1,24 @@
 class Monster:
+	#attributes
 	health=90
 	energy=40
 
+	#methods
+	def attack(self,amount):
+		print('The monster has attacked!')
+		print(f'{amount} damage was dealt')
+		monster.energy -=20
+		print(monster.energy)
+
+	def move(self,speed):
+		print('The monster has moved')
+		print(f'It has a speed of {speed}')
+
+		
+
 monster=Monster()
-print(monster.health)
-print(monster.energy)
+monster1=Monster()
+monster2=Monster()
+#monster.attack(40)
+monster.move(10)
 
