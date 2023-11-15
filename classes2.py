93e62cfa79e6c6c900577eeb92f4daef0bab580f
Meera-Ramkumar <meeraramkumar6@gmail.com>
2023-11-15 18:10:29 +0530

    'classes2'

---
+++ classes2.py
@@ -0,0 +1,30 @@
+class Monster:
+	'''A monster that has some attributes'''
+	def __init__(self,health,energy):
+		self.health=health
+		self.energy=energy
+		#private attributes
+		self._id=5
+	
+	#methods
+	def attack(self,amount):
+		print('The monster has attacked!')
+		print(f'{amount} damage was dealt')
+		self.energy-=20
+
+	def move(self,speed):
+		print('The monster has moved')
+		print(f'It has a speed of{speed}')
+monster=Monster(20,10)
+#hasattr and setattr
+#if (hasattr(monster,'health')):
+#	print(f'the monster has {monster.health} health')
+setattr(monster,'weapon','sword')
+#print(monster.weapon)
+#new_attributes=(['weapon','axe'],['armor','shield'],['potion','mana'])
+#for attr,value in new_attributes:
+#	setattr(monster,attr,value)
+#print(vars(monster))
+
+#doc
+#print(monster.__doc__)
