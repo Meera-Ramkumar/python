6e565ca689830633afb09df7774beb52b0689875
Meera-Ramkumar <meeraramkumar6@gmail.com>
2023-11-15 18:12:17 +0530

    'random_module'

---
+++ random_module.py
@@ -0,0 +1,14 @@
+import random,string
+from math import floor
+from datetime import datetime as  dt
+
+#random_number=random.randint(1,10)
+#print(random_number)
+
+#test_list=[1,2,3,4,5,6,'hello',False]
+#print(random.choice(test_list))
+
+#print(string.ascii_lowercase)
+
+#print(floor(4.9))
+print(dt.now())
